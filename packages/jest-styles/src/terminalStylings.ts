8f90af2c87acfcc8e7c557cd110cd1f8974a41c0
Ben Furber <ben@fu.rber.io>
2019-11-06 19:49:04 +0000

    ✨ Add logic to set color level through config

--- packages/jest-styles/src/terminalStylings.ts
+++ packages/jest-styles/src/terminalStylings.ts
@@ -15,7 +15,19 @@ export class TerminalStyles extends chalk.Instance {
   }
 }
 
-const level = stdout === false ? 0 : stdout.level;
+function setLevel() {
+  const config = require('../../../jest.config');
+  const styleTerminalOption = config.styleTerminal;
+
+  if (styleTerminalOption === false && styleTerminalOption !== undefined) {
+    return 0;
+  }
+
+  const defaultOption = stdout === false ? 0 : stdout.level;
+  return defaultOption;
+}
+
+const level = setLevel();
 const terminalStyles: any = new TerminalStyles({level});
 
 export default terminalStyles;
