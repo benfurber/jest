b8254b806653d67430f6417fd4b45023d8e31b14
Ben Furber <ben@fu.rber.io>
2019-11-09 17:41:24 +0000

    ♻️ Edit terminalStyles to use custom defined colors

--- packages/jest-styles/src/terminalStylings.ts
+++ packages/jest-styles/src/terminalStylings.ts
@@ -3,32 +3,49 @@ import * as chalkTypes from 'chalk';
 import {stdout} from 'supports-color';
 
 interface args {
+  colors?: colors;
   level: chalkTypes.ColorSupport['level'];
 }
 
+interface colors {
+  error: color;
+  success: color;
+  warn: color;
+}
+
+const defaultColors: colors = {
+  error: 'red',
+  success: 'green',
+  warn: 'yellow',
+};
+
 export class TerminalStyles extends chalk.Instance {
+  colors: colors;
   level: chalkTypes.ColorSupport['level'];
-  supportsColor: false | any;
+  supportsColor: false | chalkTypes.ColorSupport;
   chalk: chalkTypes.Chalk;
 
   constructor(args: args) {
     super();
+    this.colors = args.colors || defaultColors;
     this.level = args.level;
     this.supportsColor = stdout;
     this.chalk = new chalk.Instance({level: this.level});
   }
 
-  error = (string: string) => this.chalk.red(string);
-  errorBold = (string: string) => this.chalk.bold.red(string);
-  errorBanner = (string: string) => this.chalk.reset.inverse.bold.red(string);
+  error = (string: string) => this.chalk[this.colors.error](string);
+  errorBold = (string: string) => this.chalk.bold[this.colors.error](string);
+  errorBanner = (string: string) =>
+    this.chalk.reset.inverse.bold[this.colors.error](string);
 
-  success = (string: string) => this.chalk.green(string);
-  successBold = (string: string) => this.chalk.bold.green(string);
+  success = (string: string) => this.chalk[this.colors.success](string);
+  successBold = (string: string) =>
+    this.chalk.bold[this.colors.success](string);
   successBanner = (string: string) =>
-    this.chalk.reset.inverse.bold.green(string);
+    this.chalk.reset.inverse.bold[this.colors.success](string);
 
-  warn = (string: string) => this.chalk.yellow(string);
-  warnBold = (string: string) => this.chalk.bold.yellow(string);
+  warn = (string: string) => this.chalk[this.colors.warn](string);
+  warnBold = (string: string) => this.chalk.bold[this.colors.warn](string);
 }
 
 function setLevel() {
@@ -45,5 +62,26 @@ function setLevel() {
 
 const level = setLevel();
 const terminalStyles = new TerminalStyles({level});
+
+// Duplication of chalk's private color types.
+declare type color =
+  | 'black'
+  | 'red'
+  | 'green'
+  | 'yellow'
+  | 'blue'
+  | 'magenta'
+  | 'cyan'
+  | 'white'
+  | 'gray'
+  | 'grey'
+  | 'blackBright'
+  | 'redBright'
+  | 'greenBright'
+  | 'yellowBright'
+  | 'blueBright'
+  | 'magentaBright'
+  | 'cyanBright'
+  | 'whiteBright';
 
 export default terminalStyles;
